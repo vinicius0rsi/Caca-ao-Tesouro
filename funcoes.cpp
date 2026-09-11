91b00c1fa437d5b424daea19f39020346e26bf07
Vinícius José Orsi <vinicius.orsi@edu.univali.br>
2026-09-10 21:35:51 -0300

    Tratamento wasd
    Removed unused variables and commented-out code. Refactored input validation for direction and steps.

--- funcoes.cpp
+++ funcoes.cpp
@@ -8,22 +8,7 @@ using namespace std;
 
 int vetor_posicoes_jogador[2];
 int vetor_posicao_tesouro[2];
-bool verificar_num;
-bool booleano = true;
 
-struct Posicoes
-{
-    char direcao;
-    int passos;
-    int linha;
-    int coluna;
-};
-
-void verificar_valor_inteiro(Posicoes posicao, bool &booleano) {
-    if (!(isdigit(posicao.passos))) {
-        booleano = false;
-    }
-}
 
 void preencher_matriz(char matriz[TAM][TAM]) {
     for (int i = 0; i<TAM; i++){
@@ -43,40 +28,45 @@ void exibir_matriz(char matriz[TAM][TAM]) {
     }
 }
 
-
+struct Posicoes
+{
+    char direcao;
+    int passos;
+    int linha;
+    int coluna;
+};
 
 void nova_posicao(Posicoes &posicao){
 
     string pergunta_passos = "quantos passos voce deseja?: ";
-    
-    do{
-        cout << "Qual direcao voce deseja? (wasd): ";
-        cin >> posicao.direcao;
-        if(!(isalpha(posicao.direcao))){
-            cout << "\nERRO: Voce digitou um numero! \n";
-        }
 
+    do{
+    cout << "Qual direcao voce deseja? (wasd): ";
+    cin >> posicao.direcao;
+    if (!(isalpha(posicao.direcao))){
+        cout << "\nERRO: Voce digitou um numero\n";
+    }
     }while(!(isalpha(posicao.direcao)));
 
     switch(posicao.direcao) {           
         case 'w':
-    
-        while(true){
-            
-            do
-            {
+
+            while(true){
                 cout << pergunta_passos;
+                // char teste;
+                // cin >> teste;
+                // if(isalpha(teste)){
+                //     cout << "\nERRO: Voce digitou uma letra\n";
+                //     continue;
+                // } else{
+                //     posicao.passos = int(teste);
+                // }
                 cin >> posicao.passos;
-                verificar_valor_inteiro(posicao, booleano);
-                
-            } while (booleano == false);
-
-
-            posicao.linha = vetor_posicoes_jogador[0] - posicao.passos;
-            if (posicao.linha<0) {
-                cout << "ERRO: Numero de passos passou do limite! \n";
-                continue;
-            }
+                posicao.linha = vetor_posicoes_jogador[0] - posicao.passos;
+                if (posicao.linha<0) {
+                    cout << "ERRO: Numero de passos passou do limite! \n";
+                    continue;
+                }
                 break;
             }
             
@@ -160,13 +150,3 @@ void posicao_tesouro(int vetor_posicao_tesouro[2]){
 void inserir_tesouro(char matriz[TAM][TAM]){ 
     matriz[vetor_posicao_tesouro[0]][vetor_posicao_tesouro[1]] = '1';
 }
-
-//bool verificar_numero_inteiro_positivo(Posicoes posicao){
-//    bool num_inteiro_positivo = true;
-//        if(!(isdigit(posicao.passos))){
-//            num_inteiro_positivo = false;
-//            cout << "\nERRO: Voce digitou uma letra\n";
-//        }
-//    return num_inteiro_positivo;
-//}
-
